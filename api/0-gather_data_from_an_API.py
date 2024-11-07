db4f5a9e93d1815e8a09e7641621ca37d14c522f
TheSnewby <stephen.j.newby@gmail.com>
2024-11-07 16:26:56 -0600

    0-gather

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+import urllib
+import requests
+import sys
+
+employee_id = int(sys.argv[1])
+url_todos = 'https://jsonplaceholder.typicode.com/todos/'
+url_users = 'https://jsonplaceholder.typicode.com/users/'
+
+users = requests.get(url_users).json()
+for user in users:
+    if user['id'] == employee_id:
+        employee_name = user["name"]
+        break
+# print("Employee Name: {}".format(employee_name))
+todos = requests.get(url_todos).json()
+todo_count = 0
+todo_success_count = 0
+todo_list = []
+for todo in todos:
+    # print("TODO: {}".format(todo))
+    if todo['userId'] == employee_id:
+        # print("TODO: {}".format(todo))
+        if todo['completed'] == True:
+            todo_list.append(todo["title"])
+            todo_success_count += 1
+        todo_count += 1
+print("Employee {} is done with tasks({}/{}):".format(employee_name,
+      todo_success_count, todo_count))
+for todo in todo_list:
+    print("\t{}".format(todo))
