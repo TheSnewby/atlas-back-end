2c842eba096602ad4fdaea7509cb60d14613e294
TheSnewby <stephen.j.newby@gmail.com>
2024-11-07 20:56:53 -0600

    removed debug messages

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -13,26 +13,23 @@ userlist = []
 for user in users:
     userlist.append(user['username'])
 todos = requests.get(url_todos).json()  # get list of dicts of todos
-print(userlist)
+
 user_data = []  # list of todos for current user
 current_id = 0
 data_json = {}
 task_list = {}
-print("before todo")
+
 for todo in todos:  # cycles through all todos and commits user_data
     if (current_id + 1) != todo["userId"]:
         data_json[str(current_id + 1)] = user_data  # commit userdata
         current_id = todo["userId"] - 1  # set new current id
         task_list = {}  # reset vars
         user_data = []
-    print('current id: {}'.format(current_id))
     task_list['username'] = userlist[current_id]
     task_list["task"] = todo['title']
     task_list['completed'] = todo['completed']
     user_data.append(task_list)
 data_json[str(current_id + 1)] = user_data  # commits last user data
 
-print("before with open")
 with open('todo_all_employees.json', 'w', newline='') as f:
-    print('open happened')
     f.write(json.dumps(data_json))
