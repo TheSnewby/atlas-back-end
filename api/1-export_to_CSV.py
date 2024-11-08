856702c5ed681ef8aad58ec694d13fe485ebd588
TheSnewby <stephen.j.newby@gmail.com>
2024-11-07 19:37:47 -0600

    renamed file to be a variable

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -26,8 +26,7 @@ if len(sys.argv) > 1:  # ensure script has an input
             new_row = [str(employee_id), employee_name,
                        str(todo['completed']), todo['title']]
             data.append(new_row)
-            # print("DEBUG: {}".format(new_row))
 
-    with open('USER_ID.csv', 'w') as f:  # create file and write csv
+    with open('{}.csv'.format(employee_id), 'w') as f:  # csv writer
         writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerows(data)
